e1f185a4c16417d3d41868fd82fbd286f2eb0871
Lenin Bose <leninbose1596@gmail.com>
2024-07-10 23:53:30 +0530

    added another provider for habit crud state, initialized db with widget binding.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:habittracker_flutter/database/habit_database.dart';
 import 'package:habittracker_flutter/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 
 import 'pages/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // initialize db
+  await HabitDatabase.initialize();
+  await HabitDatabase().saveLaunchDate();
+
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
+    MultiProvider(
+      providers: [
+        // Habit provider
+        ChangeNotifierProvider(
+          create: (context) => HabitDatabase(),
+        ),
+        
+        // Theme provider
+        ChangeNotifierProvider(
+          create: (context) => ThemeProvider(),
+        ),
+      ],
       child: const MainApp(),
     ),
   );
