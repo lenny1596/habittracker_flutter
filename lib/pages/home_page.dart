328147f05c8d9a5650d9ad6ba4d231e0bd1cba96
Lenin Bose <leninbose1596@gmail.com>
2024-07-13 23:13:49 +0530

    added floating btn & its method to home for new habits, added init state on startup.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:habittracker_flutter/components/my_drawer.dart';
+import 'package:habittracker_flutter/database/habit_database.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,11 +12,66 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    // read existing habits on app startup
+    Provider.of<HabitDatabase>(context, listen: false).readHabits();
+    super.initState();
+  }
+
+  // floating action button method
+  TextEditingController textController = TextEditingController();
+  void createNewHabit() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+          decoration: const InputDecoration(hintText: 'Create a new habit'),
+        ),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+        ),
+        actions: [
+          // save button
+          MaterialButton(
+            onPressed: () {
+              // save new habit name
+              String newHabit = textController.text;
+              context.read<HabitDatabase>().addHabit(newHabit);
+              Navigator.pop(context);
+              textController.clear();
+            },
+            child: const Text('Save'),
+          ),
+          
+          // cancel button
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+              textController.clear();
+            },
+            child: const Text('Cancel'),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(),
       drawer: const MyDrawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewHabit,
+        backgroundColor: Theme.of(context).colorScheme.tertiary,
+        elevation: 0,
+        child: Icon(
+          Icons.add,
+          color: Theme.of(context).colorScheme.inversePrimary,
+        ),
+      ),
     );
   }
 }
